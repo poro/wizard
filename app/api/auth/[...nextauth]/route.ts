29bf61853a4a591e8c6585bcca7757072764162a
poro <mark.ollila@gmail.com>
2024-03-31 17:22:16 -0700

    Fixed auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,8 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth'
+import type { NextAuthOptions } from 'next-auth'
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    }),
-  ],
-};
-const handler = NextAuth(authOptions);
+export const authOptions: NextAuthOptions = {
+  providers: []
+}
 
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
