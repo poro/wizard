4a1f77d94b1af2533d03e03a00729166f67e8811
poro <mark.ollila@gmail.com>
2024-03-31 17:35:58 -0700

    Cleaned up auth

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from 'next-auth'
-import type { NextAuthOptions } from 'next-auth'
-
-export const authOptions: NextAuthOptions = {
-  providers: []
-}
-
-export default NextAuth(authOptions);
